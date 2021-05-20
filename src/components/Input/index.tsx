4066c3ba0aec27e450f31571145eac6af9b9a313
Lucas Diniz <lucasalvarengadiniz@gmail.com>
2021-05-20 16:46:28 -0300

    input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,10 +6,16 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
+import { IconType } from "react-icons/lib";
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps {
+  name: string;
+  icon: IconType;
+}
+
+export default function Input({ name, icon: Icon, ...rest }: InputProps) {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    setIsFilled(!!inputRef.current); // ?.value
   }, []);
 
   useEffect(() => {
@@ -48,6 +54,4 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
-};
-
-export default Input;
+}
